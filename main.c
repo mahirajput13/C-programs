99ffffb629f7fbc77b73d79d1d61d46739d3ece5
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:33:07 +0530

    conversionProgramOfMinutesIntoDaysAndYear

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
-// exampple of continue statement
+// convert number of minutes into days and years
+// minutes should be entered by the user
 
 #include <stdio.h>
 int main(void)
 {
+    float mins, days, normalyear, leapyear;
     
-    int i;
+    //taking input from the user
+    printf("\nenter the number of minutes to convert them into days, normal years and leap years : ");
+    scanf("%f", &mins);
     
-    // applying loop and continue statement
-    for(i=1; i<=5; i++)
-    {
-        if(i==3)
-        continue;
-        
-        printf("%d\n", i);
-    }
+    //applying condition
+    days = (mins/(60.0*24.0));
+    normalyear = (mins/(60.0*24.0*365.0));
+    leapyear = (mins/(60.0*24.0*366.0));
+    
+    //displaying output
+    printf("\nthe number of days in entered minutes are %f ", days);
+    printf("\nthe number of normal years in entered minuutes are %f ", normalyear);
+    printf("\nthe number of leap years in entered minutes are %f \n", leapyear);
     
     return 0;
 }
