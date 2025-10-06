7397e71809272f5b0b7267a7b1aeca87b9a900b0
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:38:59 +0530

    multiplyTwoNumbers

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
-// malloc example
+// making a program multiplying two numbers using a function
 
 #include <stdio.h>
-#include <stdlib.h>
+
+// making function to multiply two numbers
+void mul(int x, int y)
+{
+    int result = x*y;
+    printf("the multiplication of two numbers is %d \n", result);
+}
+
+//main funmction
 int main(void)
 {
+    //passing parameters and calling function
+    mul(10, 20);
+    mul(45, 1);
+    mul(3,8);
     
-    int *x = malloc(sizeof(int));
-    *x = 54;
-    free(x);
     return 0;
-}
+ }
+
+
