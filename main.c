72d9d468705f184e4683d2504af3243800f0cb10
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:44:58 +0530

    doWhileLoopExample

--- main.c
+++ main.c
@@ -1,34 +1,17 @@
-//checking and printing prime number between 3 to 100
+// print numbers from 1 to 5 using do while loop
+// example of counter controlled loop
+
 #include <stdio.h>
 int main(void)
 {
     
-    int i, num;
+    int num=1;
     
-    printf("\nprime numbers between 3 to 100 are : \n");
-    
-    //applying condition to check the number is prime or not
-    for (num=3; num<=100; num++)
-    {
-        int sum=0;
-        
-        for (i=1; i<=num; i++)
-        {
-            
-            if(num%i==0)
-            {
-                sum = sum + 1;
-            }
-            
-        }
-       
-       if(sum==2)
-       {
-           printf("%d, ", num);
-       }
-        
-    }
+    //applying condition using do while loop
+    do {
+        printf("%d\n", num);
+        num++;
+    } while (num<=5);
     
     return 0;
 }
-
