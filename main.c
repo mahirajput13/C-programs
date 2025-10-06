b101171287405d989e272d09d8a3747aeecb7911
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:30:35 +0530

    calculatesAverageOfArray

--- main.c
+++ main.c
@@ -1,18 +1,28 @@
-//argument and vector
-
+//calculate the average of the elements of an array
 #include <stdio.h>
-int main(int argc, char *argv[])
+int main (void)
 {
+    int grades[10], sum = 0, i, avg;
     
-    int numofarguments = argc;
-    char *argument1 = argv[0];
-    char *argument2 = argv[1];
+    //entering elements into the array (from the user)
+    printf("\nenter any 5 elements into the array\n");
+    for (i=0; i<5; i++)
+    {
+        scanf("%d", &grades[i]);
+    }
     
-    //displaying output
-    printf("number of arguments is %d \n", numofarguments);
-    printf("\nargument 1 is the program name : %s \n", argument1);
-    printf("\nargument 2 is the command line argument : %s \n", argument2);
+    //applying condition to get the sum of the elements of the array
+    for (i=0; i<5; i++)
+    {
+        sum = sum + grades[i];
+    }
+    
+    //finding average value
+    avg = sum/5;
+    
+    //printing output to the user
+    printf("the average value of the elements is %d \n", avg);
     
     return 0;
-}
     
+}
