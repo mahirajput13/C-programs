09f0300ea897aed78d94bb6aa693e3b86561efcb
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:48:09 +0530

    strlenFunctionExample

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-// copying one string into another using strcpy function
+// example of strlen()
+// calculate the length of thr string
 
 #include <stdio.h>
 #include <string.h>
 int main(void)
 {
-    char str1[] = "Hey! What's up?";
-    char str2[50];
     
-    //copying string 1 to string 2 and then displaying it as output
-    strcpy(str2,str1);
-    printf("the string is %s \n", str2);
+    char mystring[] = "Hey! My name is Mahi Rajput.";
+    
+    //calculate and print the lenth of the string
+    printf("the length of the string is : %lu \n", strlen(mystring));
     return 0;
+    
 }
