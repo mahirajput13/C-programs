2cf9b9686722bcd8bcddd7590fc5cec49a85a5ec
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:49:00 +0530

    strtokFunctionExample

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
-// strstr function example
-// this function searches string from another string
+// strtok function example
 
 #include <stdio.h>
 #include <string.h>
 int main(void)
 {
     
-    char str1[] = "every dog has his day";
-    char  str2[] = "dog";
-    char *pointer = NULL;
-    pointer = strstr(str1, str2);
-    printf(" %c\n", *pointer);
+    char str1[]  = " hello - how are you - my name is - jason";
+    const char str2[] = "-";
+    char *token;
+    
+    // get the first token
+    token = strtok(str1, str2);
+    
+    // walk through other tokens
+    while(token != NULL)
+    {
+        printf("%s\n", token);
+        token = strtok(NULL, str2);
+    }
     return 0;
 }
