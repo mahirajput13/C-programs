ccfc4027472d2803adfc6d503c093e57af270aab
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:45:23 +0530

    whileLoopExample

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
-// print numbers from 1 to 5 using do while loop
-// example of counter controlled loop
+//program to print number using while loop
+//example of counter controlled loop
 
 #include <stdio.h>
 int main(void)
 {
     
-    int num=1;
+    int num = 1;
     
-    //applying condition using do while loop
-    do {
+    //using while loop
+    while(num<=5)
+    {
         printf("%d\n", num);
         num++;
-    } while (num<=5);
+    }
     
     return 0;
 }
