260539e4bad55a92cf918fabfbf3b60e5b6fcdaa
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:26:52 +0530

    firstFunctionProgram
    added my first function program.

---
+++ main.c
@@ -0,0 +1,17 @@
+// first simple example of function
+
+#include <stdio.h>
+
+//making a void function
+void add(void)
+{
+}
+
+//main function
+int main(void)
+{
+    
+    add();    //calling a function
+    return 0;
+}
+
