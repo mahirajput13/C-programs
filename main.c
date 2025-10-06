5c797bc5430cca9ad19f199d9ee3612abe894411
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:28:33 +0530

    calculatesAreaPerimeterOfRectangle

--- main.c
+++ main.c
@@ -1,56 +1,25 @@
-// example of three dimensional array
-// elements of array should be entered by the user
-// print the elements of the array and the sum of all the elements of the array
+// calculate area and perimeter of rectangle
+// width and length is entered by the user
 
 #include <stdio.h>
 int main(void)
 {
+    double l,b,p,a;
     
-    int i, j, k, sum=0, ar[1][2][3];
+    //taking input
+    printf("enter the length of the rectangle : ");
+    scanf("%lf", &l);
+    printf("\nenter  the width of the rectangle : ");
+    scanf("%lf", &b);
     
-    //asking user to enter the elements into the array
-    printf("\nenter the elements into the array : \n");
-    for (i=0; i<2; i++)
-    {
-        for (j=0; j<3; j++)
-        {
-            for (k=0; k<4; k++)
-            {
-                scanf("%d ", &ar[i][j][k]);
-            }
-        }
-    }
+    //calculating values
+    p = 2.0*(l + b);
+    a = (l*b);
     
-    //printing the elements of the array
-    printf("\nthe elements of the array are : \n");
-    for (i=0; i<2; i++)
-    {
-        for (j=0; j<3; j++)
-        {
-            for (k=0; k<4; k++)
-            {
-                printf("%d", ar[i][j][k]);
-                printf("\t");
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-    
-    //calculating sum of the elements of the array
-    for (i=0; i<2; i++)
-    {
-        for (j=0; j<3; j++)
-        {
-            for (k=0; k<4; k++)
-            {
-                sum=sum+ar[i][j][k];
-            }
-        }
-    }
-    
-    //showing output of the sum to the user
-    printf("\nthe sum of the elements of the array is %d ", sum);
+    //displaying result/output
+    printf("\nperimeter of the rectangle is %lf ", p);
+    printf("\narea of the rectangle is %lf \n", a);
     
     return 0;
+    
 }
