ff9d66196ab58f3521373d6dd14708a51d284f65
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:46:48 +0530

    strcmpFunctionExample

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
-// strchr function example
-// this function searches character in a string
+// comparing two strings using strcmp function
 
 #include <stdio.h>
 #include <string.h>
 int main(void)
 {
+   
+    char str1[] = "excuse me";
+    char str2[] = "yes";
     
-    char str[] = "the quick brown fox";
-    char ch = 'u';  //character to be searched
-    char *pointer = NULL;
+    // displays 0 as output if strings are equal
+    // displays +ve value if string 1 is greater than string 2
+    // displays -ve value if string 2 is greater than string 1
     
-    pointer = strchr(str, ch);
-    printf(" %c\n", *pointer);
+    int result = strcmp(str2, str1);
+    printf("%d\n", result);
     return 0;
-    
 }
