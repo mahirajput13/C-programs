58fc7a4a9a751d515be08f0976e88ec267f87655
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:31:30 +0530

    calculatesWeeklyPay

--- main.c
+++ main.c
@@ -1,50 +1,47 @@
-// calculator program using statement
+// this program calculates your weekly pay
+// number of hours worked in a week are entered by the user
+// display the output as gross pay, the taxes, and the net pay according to given assumptions
 
 #include <stdio.h>
 int main(void)
 {
-    int a, b, add, sub, mul, div ;
-    char operator;
+    float netpay, grosspay, taxes, hours;
     
     //taking input from the user
-    printf("\nenter value 1 : ");
-    scanf("%d", &a);
-    printf("\nenter value 2 : ");
-    scanf("%d", &b);
+    printf("\nenter the number of hours worked in a week : ");
+    scanf("%f", &hours);
     
-    printf("\nselect any symbol\n\n'+' for addition\n'-' for subtraction\n'*' for multiplication\n'/' for division\n");
-    scanf(" %c",&operator);
-    
-    //applying switch cases
-    switch (operator)
+    //applying condition for calculating grosspay
+    if(hours<=40)
     {
-        case '+':
-            add = a+b;
-            printf("\naddition of entered number is %d\n", add);
-            break;
-        case '-':
-            sub = a-b;
-            printf("\nsubtraction of entered number is %d\n", sub);
-            break;
-        case '*':
-            mul = a*b;
-            printf("\nmultiplication of entered number is %d\n", mul);
-            break;
-        case '/':
-            if(b!=0)
-            {
-                div = a/b;
-                printf("\ndivision is %d\n", div);
-            }
-            else
-            {
-                printf("error : division by zero/n");
-            }
-            break;
-        default:
-            printf("\ninvalid operator\n");
-            break;
+        grosspay = (hours*12.0);
     }
+    else
+    {
+        grosspay = (40*12.0) + ((hours-40)*18);
+    }
+    
+    //applying condition for calculating taxes
+    if(grosspay<=300)
+    {
+        taxes = .15*grosspay;
+    }
+    else if(grosspay>300 && grosspay<=450)
+    {
+        taxes = (300.0*.15) + ((grosspay - 300)*.20);
+    }
+    else
+    {
+        taxes = (300.0*.15) + (150.0*.20) + ((grosspay - 450.0)*.25);
+    }
+    
+    //calculating netpay
+    netpay = grosspay - taxes;
+    
+    //displaying output
+    printf("\nyour grosspay this week is %f \n", grosspay);
+    printf("\nyour taxes this week is %f \n", taxes);
+    printf("\nyour netpay this week is %f \n\n", netpay);
     
     return 0;
 }
