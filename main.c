8526f8a108b960a2341b220bfcdfe6af6b085963
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:32:22 +0530

    continueStatementExample

--- main.c
+++ main.c
@@ -1,34 +1,19 @@
-//checks whether the number is prime or not
-//number should be entered by the user
+// exampple of continue statement
 
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-    int num, i, sum = 0;
     
-    //entering number bby the user
-    printf("\nenter any number to check whether it is prime or not  : ");
-    scanf("%d", &num);
+    int i;
     
-    //applying condition to check whether it is prime or not
-    for (i=1; i<=num; i++)
+    // applying loop and continue statement
+    for(i=1; i<=5; i++)
     {
-        if(num%i==0)
-        {
-            sum = sum + 1;
-        }
+        if(i==3)
+        continue;
+        
+        printf("%d\n", i);
     }
-    
-    //printing the output to the user after checking
-    if(sum==2)
-    {
-        printf("\nthe entered number is prime\n");
-    }
-    else
-    {
-        printf("\nthe entered number is not prime\n");
-    }
-    
     
     return 0;
 }
