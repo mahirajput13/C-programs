595490deb2b4c2d611b18a3f4809cdfd2845c1f7
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:37:21 +0530

    escapeSequenceExample

--- main.c
+++ main.c
@@ -1,40 +1,11 @@
-// example of both enum and switch case
+// example of aan escape sequence
 
 #include <stdio.h>
 int main(void)
+
 {
-    //defining variables
-    enum weekday{monday, tuesday, wednesday, thursday, friday, saturday, sunday};
-    enum weekday today = monday;
-    
-    //applying switch cases
-    switch (today)
-    {
-        case sunday:
-            printf("today is sunday\n");
-            break;
-        case monday:
-            printf("today is monday\n");
-            break;
-        case tuesday:
-            printf("today is tuesday\n");
-            break;
-        case wednesday:
-            printf("today is wednesday\n");
-            break;
-        case thursday:
-            printf("today is thursday\n");
-            break;
-        case friday:
-            printf("today is friday\n");
-            break;
-        case saturday:
-            printf("today is saturday\n");
-            break;
-        default:
-            printf("whatever\n");
-            break;
-    }
-    
+    //backslash operator
+    printf("for \"you write\\\".\n");h
     return 0;
+    
 }
