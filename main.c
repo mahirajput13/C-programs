c9fbf990254a05c7548f7c0e73554cd2556e29e1
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:42:25 +0530

    secondPointerProgram

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
-// this is my first pointer program
+// this is my second pointer program
+
 #include <stdio.h>
 #include <stdlib.h>
 int main(void)
 {
-    int count = 10, x;
-    int *pointer = NULL;
-    pointer = &count;
-    x = *pointer;
     
-    printf("count = %i, x = %i\n", count, x);
+    int number = 0;
+    int *pnum = NULL;
+    
+    number = 10;
+    
+    printf("the number's value is %d\n", number);
+    printf("the number's address is %p\n", &number);
+    
+    pnum = &number;
+    
+    printf("pnum's address is %p \n", pnum);
+    printf("pnum's value is %d \n", *pnum);
+    printf("pnum's size is %zd bytes \n", sizeof(pnum));
+    
     return 0;
 }
