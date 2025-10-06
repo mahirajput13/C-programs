b5e2da60965ed1982daff5c4b07365ae02259b7e
Mahi Rajput <Mahirajput9528@gmail.com>
2025-10-06 19:40:53 +0530

    passbyReferenceExample

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
-// determine if the number entered by user is even or odd
-
+// example of pass by reference
 #include <stdio.h>
+#include <stdlib.h>
+
+//creating function to swap the value
+void swap(int *x, int *y)
+{
+    int temp;
+    
+    temp = *x;
+    *x = *y;
+    *y = temp;
+    
+    return;
+}
+
+//creating main function
 int main(void)
 {
-    int num;
     
-    //taking input from the user
-    printf("\nenter any number to check if it is even or odd : ");
-    scanf("%d", &num);
+    int a = 100, b=200;
     
-    //applying condition and displaying output
-    if((num%2)==0)
-    {
-        printf("\nnumber entered by you is even\n");
-    }
-    else
-    {
-        printf("number entered by you is odd\n");
-    }
+    //printing values before swapping
+    printf("before swapping\n");
+    printf("the value of a %d\n",a);
+    printf("the value of b %d\n",b);
+    
+    printf("\nafter swapping\n");
+    
+    //calling swap function
+    swap(&a,&b);
+    printf("the value of a %d\n",a);
+    printf("the value of b %d\n",b);
     
     return 0;
+    
 }
